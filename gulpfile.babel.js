9a59a21f53c5f031c31061d007c43748b2e80383
coseo12 <momo_12@naver.com>
2020-10-20 17:45:27 +0900

    init

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,11 @@ import del from 'del';
 import ws from 'gulp-webserver';
 import image from 'gulp-image';
 import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import microCSS from 'gulp-csso';
+import bro from 'gulp-bro';
+import babelify from 'babelify';
+import ghPages from 'gulp-gh-pages';
 
 sass.compiler = require('node-sass');
 
@@ -22,12 +27,17 @@ const routes = {
     src: 'src/scss/style.scss',
     dest: 'build/css',
   },
+  js: {
+    watch: 'src/js/**/*.js',
+    src: 'src/js/main.js',
+    dest: 'build/js',
+  },
 };
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(['build/']);
+const clean = () => del(['build/', '.publish/']);
 
 const webserver = () =>
   gulp.src('build').pipe(
@@ -44,18 +54,42 @@ const styles = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+      })
+    )
+    .pipe(microCSS())
     .pipe(gulp.dest(routes.scss.dest));
+
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({
+            presets: ['@babel/preset-env'],
+          }),
+          ['uglifyify', { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
+const gh = () => gulp.src('build/**/*').pipe(ghPages());
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
 };
 
-const prepare = gulp.series([clean, img]);
-
-const assets = gulp.series([pug, styles]);
-
+const prepare = gulp.series([clean]);
+const assets = gulp.series([pug, styles, img, js]);
 const postDev = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, postDev]);
+export const deploy = gulp.series([build, gh, clean]);
